effa84f53a0721cafce6c99c1568467084e38fd4
root <root@a0d7b954-vscode.local.hass.io>
2020-08-27 21:28:24 +0200

    Fixed JS

--- miflora-card.js
+++ miflora-card.js
@@ -1,4 +1,4 @@
-console.info("%c  MIFLORA-CARD  \n%c Version 0.1.2 ", "color: orange; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
+/* miflora-card - version: v0.1.0 */
 class MifloraCard extends HTMLElement {
     constructor() {
         super();
@@ -49,10 +49,10 @@ class MifloraCard extends HTMLElement {
     set hass(hass) {
         const config = this.config;
 
-        var _maxMoisture = parseFloat(config.max_moisture);
-        var _minMoisture = parseFloat(config.min_moisture);
-        var _minConductivity = parseFloat(config.min_conductivity);
-        var _minTemperature = parseFloat(config.min_termperature);
+        var _maxMoisture = config.max_moisture;
+        var _minMoisture = config.min_moisture;
+        var _minConductivity = config.min_conductivity;
+        var _minTemperature = config.min_termperature;
 
         this.shadowRoot.getElementById('container').innerHTML = `
             <div class="content clearfix">
@@ -61,26 +61,22 @@ class MifloraCard extends HTMLElement {
             `;
 
         for (var i = 0; i < config.entities.length; i++) {
-            var _name = config.entities[i]['type'];
-            var _sensor = config.entities[i]['entity'];
-            if (config.entities[i]['name']) {
-                var _display_name = config.entities[i]['name'];
-            } else {
-                var _display_name = _name[0].toUpperCase() + _name.slice(1);
-            }
+            var _type = config.entities[i].type;
+            var _sensor = config.entities[i].entity;
             var _state = '';
             var _uom = '';
             if (hass.states[_sensor]) {
-                _state = parseFloat(hass.states[_sensor].state);
+                _state = hass.states[_sensor].state;
                 _uom = hass.states[_sensor].attributes.unit_of_measurement || "";
             } else {
                 _state = 'Invalid Sensor';
             }
 
-            var _icon = this._computeIcon(_name, _state);
+            var _icon = this._computeIcon(_type, _state);
+            var _name = config.entities[i].name ? config.entities[i].name : _type;
             var _alertStyle = '';
             var _alertIcon = '';
-            if (_name == 'moisture') {
+            if (_type == 'moisture') {
                 if (_state > _maxMoisture) {
                     _alertStyle = ';color:red';
                     _alertIcon = '&#9650; ';
@@ -89,13 +85,13 @@ class MifloraCard extends HTMLElement {
                     _alertIcon = '&#9660; '
                 }
             }
-            if (_name == 'conductivity') {
+            if (_type == 'conductivity') {
                 if (_state < _minConductivity) {
                     _alertStyle = ';color:red';
                     _alertIcon = '&#9660; ';
                 }
             }
-            if (_name == 'temperature') {
+            if (_type == 'termperature') {
                 if (_state < _minTemperature) {
                     _alertStyle = ';color:red';
                     _alertIcon = '&#9660; ';
@@ -104,14 +100,14 @@ class MifloraCard extends HTMLElement {
             this.shadowRoot.getElementById('sensors').innerHTML += `
                 <div id="sensor${i}" class="sensor">
                     <div class="icon"><ha-icon icon="${_icon}"></ha-icon></div>
-                    <div class="name">${_display_name[0].toUpperCase()}${_display_name.slice(1)}</div>
+                    <div class="name">${_name}</div>
                     <div class="state" style="${_alertStyle}">${_alertIcon}${_state}${_uom}</div>
                 </div>
                 `
         }
 
         for (var i = 0; i < config.entities.length; i++) {
-            this.shadowRoot.getElementById('sensor' + [i]).onclick = this._click.bind(this, config.entities[i]['entity']);
+            this.shadowRoot.getElementById('sensor' + [i]).onclick = this._click.bind(this, config.entities[i].entity);
         }
     }
 
@@ -133,34 +129,35 @@ class MifloraCard extends HTMLElement {
 
         style.textContent = `
             ha-card {
-                position: relative;
-                padding: 0;
-                background-size: 100%;
+            position: relative;
+            padding: 0;
+            background-size: 100%;
             }
             ha-card .header {
-                width: 100%;
+            width: 100%;
             }
             .image {
                 float: right;
-                margin-left: 15px;
-                margin-right: 15px;
-                margin-bottom: 15px;
-                width: 125px;
-                height: 125px;
+                margin-left: 16px;
+                margin-right: 16px;
+                margin-bottom: 16px;
+                width: 155px;
+                height: 155px;
                 border-radius: 6px;
             }
             .sensor {
+                padding-top: 10px;
+                padding-bottom: 10px;
+                margin-right: 16px;
                 display: flex;
                 cursor: pointer;
-                padding-bottom: 10px;
             }
             .icon {
-                margin-left: 10px;
                 color: var(--paper-item-icon-color);
             }
             .name {
                 margin-top: 3px;
-                margin-left: 10px;
+                margin-left: 16px;
                 white-space: nowrap;
                 overflow: hidden;
                 text-overflow: ellipsis;
@@ -181,13 +178,14 @@ class MifloraCard extends HTMLElement {
             }
             `;
         plantimage.innerHTML = `
-            <img class="image" src=/local/${config.image}>  
+            <img class="image" src=${config.image}>  
             `;
 
         content.id = "container";
         card.header = config.title;
         card.appendChild(plantimage);
         card.appendChild(content);
+        content.setAttribute("class", "card-content");
         card.appendChild(style);
         root.appendChild(card);
     }
